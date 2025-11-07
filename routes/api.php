c1287fdac500f2678e808b24d86473d8d550e178
LUW72 <schneider.ivan337@gmail.com>
2025-11-07 16:28:20 +0100

    api javitva

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
-=======
-use App\Http\Controllers\LendingController;
->>>>>>> 4fb75fe062fc5398ee306474d394d4e905998750
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\Admin;
 use Illuminate\Http\Request;
@@ -35,17 +31,3 @@ Route::middleware(['auth:sanctum', Admin::class])->group(function ()
     Route::get('/users', [UserController::class, 'index']);
 });
 
-<<<<<<< HEAD
-=======
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
-
-Route::get('/lendings', [LendingController::class, 'index']);
-Route::get('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'show']);
-Route::post('/lendings', [LendingController::class, 'store']);
-Route::put('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'update']);
-Route::delete('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'destroy']);
->>>>>>> 4fb75fe062fc5398ee306474d394d4e905998750
